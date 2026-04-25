1e51747b662ed6fe7d2c237aa04dbdf8e9bd92a0
adiwaruwu <adiputrawaruwu030303@gmail.com>
2026-04-25 18:07:43 +0700

    fix: remove problematic empty page causing build error

--- app/more/page.tsx
+++
@@ -1,6 +0,0 @@
-'use client'
-
-import { Header } from '@/components/header'
-import { Footer } from '@/components/footer'
-import Link from 'next/link'
-import { Button } from '@/components/ui/button'
